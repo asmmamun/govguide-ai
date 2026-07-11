266f616a11d1440f4722fcde905392b7c3acacd9
A S M Mamun <uolmamun@gmail.com>
2026-07-11 19:39:04 +0600

    Add preprocessing module and integrate pipeline

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from src.ingestion.loader import load_documents
-
+from src.preprocessing.preprocessor import preprocess_documents
 print("Main is running")
+
+documents = load_documents("data/raw")
+preprocessed_docs = preprocess_documents(documents)
+
